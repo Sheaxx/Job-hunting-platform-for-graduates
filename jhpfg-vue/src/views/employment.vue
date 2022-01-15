899566d7dd5777048086f5d4539b6edba43abd3f
Sheaxx <61724225+Sheaxx@users.noreply.github.com>
2022-01-15 14:28:49 +0800

    招聘详情页（没做完）

--- jhpfg-vue/src/views/employment.vue
+++ jhpfg-vue/src/views/employment.vue
@@ -2,85 +2,114 @@
   <div id="employment">
     <i class="el-icon-s-flag index-icon" slot="icon"></i>
     <h4 class="index-boxTitle" slot="boxTitle">招聘信息</h4>
-    <el-input
-      placeholder="请输入内容"
-      v-model="searchValue"
-      class="input-with-select searchInput"
-    >
-      <el-select v-model="searchSelect" slot="prepend" placeholder="请选择">
-        <el-option label="搜岗位" value="1"></el-option>
-        <el-option label="搜公司" value="2"></el-option>
-      </el-select>
-      <el-button slot="append" icon="el-icon-search" class="searchButton"
-        >找全职</el-button
+    <div id="employmentListBox" v-if="!isDetails">
+      <el-input
+        placeholder="请输入内容"
+        v-model="searchValue"
+        class="input-with-select searchInput"
       >
-      <el-button slot="append" icon="el-icon-search" class="searchButton"
-        >找实习</el-button
+        <el-select v-model="searchSelect" slot="prepend" placeholder="请选择">
+          <el-option label="搜岗位" value="1"></el-option>
+          <el-option label="搜公司" value="2"></el-option>
+        </el-select>
+        <el-button slot="append" icon="el-icon-search" class="searchButton"
+          >找全职</el-button
+        >
+        <el-button slot="append" icon="el-icon-search" class="searchButton"
+          >找实习</el-button
+        >
+      </el-input>
+      <ul id="filter">
+        <li>筛选岗位：</li>
+        <li>
+          <el-select v-model="educationFilter" placeholder="学历要求">
+            <el-option value="不限"></el-option>
+            <el-option value="大专"></el-option>
+            <el-option value="本科"></el-option>
+            <el-option value="硕士"></el-option>
+            <el-option value="博士"></el-option>
+          </el-select>
+        </li>
+        <li>
+          <el-select v-model="salaryFilter" placeholder="薪资待遇">
+            <el-option value="不限"></el-option>
+            <el-option value="3K以下"></el-option>
+            <el-option value="3-5K"></el-option>
+            <el-option value="5-10K"></el-option>
+            <el-option value="10-20K"></el-option>
+            <el-option value="20K以上"></el-option>
+          </el-select>
+        </li>
+        <li>
+          <el-cascader
+            :options="locationOptions"
+            v-model="locationSelectedOptions"
+            placeholder="工作地点"
+          >
+          </el-cascader>
+        </li>
+        <li>
+          <el-switch
+            v-model="certification"
+            active-text="只看学校认证"
+            inactive-text="展示所有岗位"
+          >
+          </el-switch>
+        </li>
+        <li>
+          <el-button round icon="el-icon-refresh-left">重置</el-button>
+        </li>
+      </ul>
+      <ul id="employmentList">
+        <li
+          class="employmentItem"
+          v-for="item in employmentList"
+          :key="item.id"
+        >
+          <p class="salary">{{ item.salary }}</p>
+          <p class="station">{{ item.station }}</p>
+          <p class="jobMsg">
+            <span>{{ item.location }}</span>
+            <span>{{ item.education }}</span>
+          </p>
+          <p class="companyMsg">
+            <span>{{ item.company }}</span>
+            <span>{{ item.trade }}</span>
+            <span>{{ item.level }}</span>
+          </p>
+        </li>
+      </ul>
+      <el-pagination
+        :page-size="20"
+        :pager-count="11"
+        layout="prev, pager, next"
+        :total="1000"
       >
-    </el-input>
-    <ul id="filter">
-      <li>筛选岗位：</li>
-      <li>
-        <el-select v-model="educationFilter" placeholder="学历要求">
-          <el-option value="不限"></el-option>
-          <el-option value="大专"></el-option>
-          <el-option value="本科"></el-option>
-          <el-option value="硕士"></el-option>
-          <el-option value="博士"></el-option>
-        </el-select>
-      </li>
-      <li>
-        <el-select v-model="salaryFilter" placeholder="薪资待遇">
-          <el-option value="不限"></el-option>
-          <el-option value="3K以下"></el-option>
-          <el-option value="3-5K"></el-option>
-          <el-option value="5-10K"></el-option>
-          <el-option value="10-20K"></el-option>
-          <el-option value="20K以上"></el-option>
-        </el-select>
-      </li>
-      <li>
-        <el-cascader
-          :options="locationOptions"
-          v-model="locationSelectedOptions"
-          placeholder="工作地点"
-        >
-        </el-cascader>
-      </li>
-      <li>
-        <el-switch
-          v-model="certification"
-          active-text="只看学校认证"
-          inactive-text="展示所有岗位"
-        >
-        </el-switch>
-      </li>
-      <li>
-        <el-button round icon="el-icon-refresh-left">重置</el-button>
-      </li>
-    </ul>
-    <ul id="employmentList">
-      <li class="employmentItem" v-for="item in employmentList" :key="item.id">
-        <p class="salary">{{ item.salary }}</p>
-        <p class="station">{{ item.station }}</p>
+      </el-pagination>
+    </div>
+    <div id="employmentDetailsBox" v-else>
+      <el-button round @click="" class="toList">返回</el-button>
+      <div class="topBar">
+        <h5>{{ details.station }}</h5>
+        <h6>{{ details.salary }}</h6>
         <p class="jobMsg">
-          <span>{{ item.location }}</span>
-          <span>{{ item.education }}</span>
+          <span>{{ details.location }}</span>
+          <span>{{ details.education }}</span>
         </p>
-        <p class="companyMsg">
-          <span>{{ item.company }}</span>
-          <span>{{ item.trade }}</span>
-          <span>{{ item.level }}</span>
-        </p>
-      </li>
-    </ul>
-    <el-pagination
-      :page-size="20"
-      :pager-count="11"
-      layout="prev, pager, next"
-      :total="1000"
-    >
-    </el-pagination>
+      </div>
+      <div class="introduction section">
+        <h5 class="sectionTitle"><i class="el-icon-suitcase sectionIcon"></i>职位介绍</h5>
+        <p class="sectionContent">{{details.introduction}}</p>
+      </div>
+      <div class="requirements section">
+        <h5 class="sectionTitle"><i class="el-icon-notebook-2 sectionIcon"></i>职位要求</h5>
+        <p class="sectionContent">{{details.requirements}}</p>
+      </div>
+      <div class="companyBar">
+        <h5>公司基本信息</h5>
+        
+      </div>
+    </div>
   </div>
 </template>
 
@@ -94,14 +123,16 @@ import {
 export default {
   data() {
     return {
-      searchValue: "",
-      searchSelect: "",
-      educationFilter: "",
-      salaryFilter: "",
+      isDetails: false, //是否是详情页，默认为否
+      searchValue: "", //搜索内容
+      searchSelect: "", //选择搜索的类别
+      educationFilter: "", //筛选学历
+      salaryFilter: "", //筛选薪资
       locationOptions: provinceAndCityDataPlus,
-      locationSelectedOptions: [],
-      certification: false,
+      locationSelectedOptions: [], //工作地点选择的两个变量
+      certification: false, //筛选学校认证
       employmentList: [
+        //首页招聘信息列表
         {
           employmentid: 1,
           isFullTime: true,
@@ -192,15 +223,18 @@ export default {
         },
       ],
       details: {
-        employmentid: "",
-        isFullTime: false,
-        station: "",
-        salary: "",
-        location: "",
-        education: "",
-        company: "",
-        trade: "",
-        level: "",
+        //某个招聘信息的详情
+        employmentid: 1,
+        isFullTime: true,
+        station: "高级产品经理",
+        salary: "10-20k",
+        location: "北京",
+        education: "本科",
+        company: "字节跳动",
+        trade: "互联网",
+        level: "已上市",
+        introduction:"1、对国家政策、产业环境、市场规模等进行洞察，结合客户需求进行痛点分析，聚焦功能需求，适配相关解决方案。2、根据市场洞察，结合客户需求，孵化数字政府相关解决方案，协同合作伙伴进行落地支撑；3、进行产业环境分析，对服务区域的主导产业进行分析研究，结合方案进行信息化平台的售前工作，4、根据项目需求进行实地调研，可独立输出PPT以及WORD等相关报告，能够适应中短期出差。",
+        requirements:"1、具备良好的沟通表达能力和组织协调能力，懂政府语言，团队意识强，抗压能力强，有激情，勇于接受挑战，善于开拓创新。2、至少精通生物医药、新一代信息技术等战新产业体系中的一个，熟悉行业领域格局。可独立宇客户进行业务交流。具备需求调研、服务解决方案设计能力。3、5年以上车联网、人工智能、生物医药、文旅行业领域从业经验，过往工作经历中有产业研究，信息化咨询、解决方案售前等相关工作经验，获得相关证书者优先。"
       },
     };
   },
@@ -208,6 +242,9 @@ export default {
 </script>
 
 <style>
+#employment {
+  position: relative;
+}
 .searchInput {
   font-size: 0.9rem !important;
   margin-top: 3vh;
@@ -278,5 +315,52 @@ export default {
 }
 .el-pagination {
   text-align: center;
+}
+.toList {
+  float: right;
+}
+.topBar {
+  clear: both;
+  width: 100%;
+}
+.topBar h5 {
+  float: left;
+  margin-right: 4vw;
+  font-size: 1.5rem;
+}
+.topBar h6 {
+  font-size: 1.5rem;
+  color: #72b3f0;
+}
+.topBar .jobMsg {
+  margin-top: 2vh;
+}
+.topBar .jobMsg span {
+  margin-right: 1vw;
+  color: #8e9094;
+}
+.section {
+  margin-top: 5vh;
+  width: 70%;
+}
+.sectionTitle {
+  font-size: 1.1rem;
+  color: #2E4E74;
+  font-weight: 700;
+  margin-bottom: 2vh;
+}
+.sectionIcon {
+  margin-right: 0.5vw;
+}
+.sectionContent {
+  line-height: 1.6rem;
+}
+.companyBar {
+  position: fixed;
+  width: 25%;
+  height: 70vh;
+  background: red;
+  top: 25vh;
+  left: 71vw;
 }
 </style>
--- jhpfg-vue/src/views/forum.vue
+++ jhpfg-vue/src/views/forum.vue
@@ -2,6 +2,7 @@
   <div id="forum">
     <i class="el-icon-s-comment index-icon" slot="icon"></i>
     <h4 class="index-boxTitle" slot="boxTitle">论坛</h4>
+    
   </div>
 </template>
 
